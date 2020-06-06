195b6a72dc38ebdcefba78d59a9c3f2c761f82c5
Dou Jones <misterchiefexecutiveofficer@yahoo.com>
2020-06-06 15:19:34 -0400

    first commit

---
+++ dist/products/build/precache-manifest.f1876892072054fbbb9eb5c2ae90921d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1ae9fa884336ff4e7639",
+    "url": "/static/js/main.1ae9fa88.chunk.js"
+  },
+  {
+    "revision": "033f96ce5900b78e8fca",
+    "url": "/static/js/1.033f96ce.chunk.js"
+  },
+  {
+    "revision": "1ae9fa884336ff4e7639",
+    "url": "/static/css/main.52069125.chunk.css"
+  },
+  {
+    "revision": "033f96ce5900b78e8fca",
+    "url": "/static/css/1.254f5924.chunk.css"
+  },
+  {
+    "revision": "e53ec122464f86abd7b3830caaa25a68",
+    "url": "/index.html"
+  }
+];
